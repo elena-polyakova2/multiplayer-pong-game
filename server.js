700cd0ab3a7a35297486027b2e409385f05c775e
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-11-05 14:07:38 -0500

    #7. Listening for events in the Pong server.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- server.js
+++ server.js
@@ -11,6 +11,20 @@ const PORT = 3000;
 server.listen(PORT);
 console.log(`Listening on port ${PORT}...`);
 
+let readyPlayerCount = 0;
+
 io.on('connection', (socket) => {
   console.log('User connected as ', socket.id);
+
+  //notify when a player found
+  socket.on('ready', () => {
+    console.log('A player with id# ', socket.id,' is ready to play');
+
+    readyPlayerCount++;
+
+    if(readyPlayerCount === 2) {
+      //broadcast 'start game event'
+
+    }
+  })
 });
