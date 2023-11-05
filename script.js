1d7e160175fbf24fa6c17214dcf2957a70605581
Elena-Polyakova <72936523+Elena-Polyakova@users.noreply.github.com>
2023-11-05 15:08:07 -0500

    #9. Handling events in the Pong client.
    Co-authored-by: elena-polyakova2 <124845955+elena-polyakova2@users.noreply.github.com>

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// Canvas Related 
+//Canvas Related 
 const canvas = document.createElement('canvas');
 const context = canvas.getContext('2d');
 const socket = io('http://localhost:3000');
+let isReferee = false;
 let paddleIndex = 0;
 
 let width = 500;
 let height = 700;
 
-// Paddle
+//Paddle
 let paddleHeight = 10;
 let paddleWidth = 50;
 let paddleDiff = 25;
@@ -15,20 +16,20 @@ let paddleX = [ 225, 225 ];
 let trajectoryX = [ 0, 0 ];
 let playerMoved = false;
 
-// Ball
+//Ball
 let ballX = 250;
 let ballY = 350;
 let ballRadius = 5;
 let ballDirection = 1;
 
-// Speed
+//Speed
 let speedY = 2;
 let speedX = 0;
 
-// Score for Both Players
+//Score for Both Players
 let score = [ 0, 0 ];
 
-// Create Canvas Element
+//Create Canvas Element
 function createCanvas() {
   canvas.id = 'canvas';
   canvas.width = width;
@@ -37,34 +38,34 @@ function createCanvas() {
   renderCanvas();
 }
 
-// Wait for Opponents
+//Wait for Opponents
 function renderIntro() {
-  // Canvas Background
+  //Canvas Background
   context.fillStyle = 'black';
   context.fillRect(0, 0, width, height);
 
-  // Intro Text
+  //Intro Text
   context.fillStyle = 'white';
   context.font = "32px Courier New";
   context.fillText("Waiting for opponent...", 20, (canvas.height / 2) - 30);
 }
 
-// Render Everything on Canvas
+//Render Everything on Canvas
 function renderCanvas() {
-  // Canvas Background
+  //Canvas Background
   context.fillStyle = 'darkgreen';
   context.fillRect(0, 0, width, height);
 
-  // Paddle Color
+  //Paddle Color
   context.fillStyle = 'azure';
 
-  // Bottom Paddle
+  //Bottom Paddle
   context.fillRect(paddleX[0], height - 20, paddleWidth, paddleHeight);
 
-  // Top Paddle
+  //Top Paddle
   context.fillRect(paddleX[1], 10, paddleWidth, paddleHeight);
 
-  // Dashed Center Line
+  //Dashed Center Line
   context.beginPath();
   context.setLineDash([4]);
   context.moveTo(0, 350);
@@ -72,52 +73,52 @@ function renderCanvas() {
   context.strokeStyle = 'white';
   context.stroke();
 
-  // Ball
+  //Ball
   context.beginPath();
   context.arc(ballX, ballY, ballRadius, 2 * Math.PI, false);
   context.fillStyle = 'yellow';
   context.fill();
 
-  // Score
+  //Score
   context.font = "32px Courier New";
   context.fillText(score[0], 20, (canvas.height / 2) + 50);
   context.fillText(score[1], 20, (canvas.height / 2) - 30);
 }
 
-// Reset Ball to Center
+//Reset Ball to Center
 function ballReset() {
   ballX = width / 2;
   ballY = height / 2;
   speedY = 3;
 }
 
-// Adjust Ball Movement
+//Adjust Ball Movement
 function ballMove() {
-  // Vertical Speed
+  //Vertical Speed
   ballY += speedY * ballDirection;
-  // Horizontal Speed
+  //Horizontal Speed
   if (playerMoved) {
     ballX += speedX;
   }
 }
 
-// Determine What Ball Bounces Off, Score Points, Reset Ball
+//Determine What Ball Bounces Off, Score Points, Reset Ball
 function ballBoundaries() {
-  // Bounce off Left Wall
+  //Bounce off Left Wall
   if (ballX < 0 && speedX < 0) {
     speedX = -speedX;
   }
-  // Bounce off Right Wall
+  //Bounce off Right Wall
   if (ballX > width && speedX > 0) {
     speedX = -speedX;
   }
-  // Bounce off player paddle (bottom)
+  //Bounce off player paddle (bottom)
   if (ballY > height - paddleDiff) {
     if (ballX >= paddleX[0] && ballX <= paddleX[0] + paddleWidth) {
-      // Add Speed on Hit
+      //Add Speed on Hit
       if (playerMoved) {
         speedY += 1;
-        // Max Speed
+        //Max Speed
         if (speedY > 5) {
           speedY = 5;
         }
@@ -126,18 +127,18 @@ function ballBoundaries() {
       trajectoryX[0] = ballX - (paddleX[0] + paddleDiff);
       speedX = trajectoryX[0] * 0.3;
     } else {
-      // Reset Ball, add to Computer Score
+      //Reset Ball, add to Computer Score
       ballReset();
       score[1]++;
     }
   }
-  // Bounce off computer paddle (top)
+  //Bounce off computer paddle (top)
   if (ballY < paddleDiff) {
     if (ballX >= paddleX[1] && ballX <= paddleX[1] + paddleWidth) {
-      // Add Speed on Hit
+      //Add Speed on Hit
       if (playerMoved) {
         speedY += 1;
-        // Max Speed
+        //Max Speed
         if (speedY > 5) {
           speedY = 5;
         }
@@ -152,7 +153,7 @@ function ballBoundaries() {
   }
 }
 
-// Called Every Frame
+//Called Every Frame
 function animate() {
   ballMove();
   renderCanvas();
@@ -160,13 +161,16 @@ function animate() {
   window.requestAnimationFrame(animate);
 }
 
-// Start Game, Reset Everything
-function startGame() {
+//Load Game, Reset Everything
+function loadGame() {
   createCanvas();
   renderIntro();
   socket.emit('ready');
-  
-  paddleIndex = 0;
+}
+
+function startGame() {
+  //set the bottom paddle for the referee ans the top paddle for non-referee player
+  paddleIndex = isReferee ? 0 : 1;
   window.requestAnimationFrame(animate);
   canvas.addEventListener('mousemove', (e) => {
     playerMoved = true;
@@ -177,14 +181,21 @@ function startGame() {
     if (paddleX[paddleIndex] > (width - paddleWidth)) {
       paddleX[paddleIndex] = width - paddleWidth;
     }
-    // Hide Cursor
+    //Hide Cursor
     canvas.style.cursor = 'none';
   });
 }
 
-// On Load
-startGame();
+//On Load
+loadGame();
 //get connection id
 socket.on('connect', () => {
   console.log('Connected as ', socket.id);
 });
+
+socket.on('startGame', (refereeId) => {
+  console.log('The referee is ', refereeId);
+
+  isReferee = socket.id === refereeId;
+  startGame();
+})
